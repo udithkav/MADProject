a6236e69955248b35bec2d78da19ac0197dc85ad
Udith Kavisinghe <it19206660@my.sliit.lk>
2020-08-16 23:26:16 +0530

    Completed Chart

--- app/src/main/java/com/example/humanresourcemanager/HumanResourceHomePage.java
+++ app/src/main/java/com/example/humanresourcemanager/HumanResourceHomePage.java
@@ -4,11 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HumanResourceHomePage extends AppCompatActivity {
+
+    int totalNumberofTourGuidesAccepted[] = {1,4,5,6,2,3,1,3,0,7,3,1};
+    String month[] = {"Jan","Feb","Mar","Apr","May","June","Jul","Aug","Sept","Oct","Nov","Dec"};
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_human_resource_home_page);
+        
+        setupPieChart();
+        
+    }
+
+    private void setupPieChart() {
+        List<PieEntry> PieEntries = new ArrayList<>();
+
+        for(int i = 0 ; i <totalNumberofTourGuidesAccepted.length; i++){
+            PieEntries.add(new PieEntry(totalNumberofTourGuidesAccepted[i],month[i]));
+        }
+        PieDataSet dataSet = new PieDataSet(PieEntries,"Number of Tourguides Accepted/Declined");
+        PieData data = new PieData(dataSet);
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        PieChart chart = (PieChart)findViewById(R.id.chart);
+        chart.setData(data);
+        chart.invalidate();
     }
 }
