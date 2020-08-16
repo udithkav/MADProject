c2c26d798884b3a94863dc0e8cf69b830e6d322a
Udith Kavisinghe <it19206660@my.sliit.lk>
2020-08-16 16:01:49 +0530

    Updated Background, Fixed scaling issues, Added Static strings.xml

--- app/src/main/java/com/example/humanresourcemanager/hrApplicationPreview.java
+++ app/src/main/java/com/example/humanresourcemanager/hrApplicationPreview.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ApplicationPreviewHR extends AppCompatActivity {
+public class hrApplicationPreview extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.activity_hr_application_preview);
     }
 }
